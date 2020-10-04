f425c9dacfae5c8005a3894f3b0bbfc2c4517a0e
Islam Madkaur <islammadkaur@gmail.com>
2020-10-04 01:16:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  random_array = [food, water, money, fun]
+  random_array = ["food", "water", "money", "fun"]
 end
 
 def add_element_to_end_of_array(array, element)
-  random_array = [food, water, money, fun]
+  random_array = ["food", "water", "money", "fun"]
   random_array.push(element)
 end
 
